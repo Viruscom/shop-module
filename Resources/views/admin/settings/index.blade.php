86617f1482c16d22215a176af0be617406423c31
Viruscom <hristiqn_chobanov@abv.bg>
2023-02-18 11:36:27 +0200

    123123

--- Resources/views/admin/settings/index.blade.php
+++ Resources/views/admin/settings/index.blade.php
@@ -1,26 +1,8 @@
 @extends('layouts.admin.app')
 @section('styles')
-    <link href="{{ asset('admin/assets/css/select2.min.css') }}" rel="stylesheet"/>
+    <link href="{{ asset('admin/assets/css/shop.css') }}" rel="stylesheet"/>
 @endsection
-@section('scripts')
-    <script src="{{ asset('admin/assets/js/select2.min.js') }}"></script>
-    <script src="{{ asset('admin/assets/js/bootstrap-confirmation.js') }}"></script>
-    <script>
-        $('[data-toggle=confirmation]').confirmation({
-            rootSelector: '[data-toggle=confirmation]',
-            container: 'body',
-        });
-        $(".select2").select2({language: "bg"});
 
-        $(document).ready(function () {
-            $('[data-toggle="popover"]').popover({
-                placement: 'auto',
-                trigger: 'hover',
-                html: true
-            });
-        });
-    </script>
-@endsection
 @section('content')
     @include('admin.notify')
 
@@ -28,10 +10,10 @@
     <div class="row">
         <div class="col-xs-12">
             <h3>@lang('shop::admin.shop.settings_index')</h3>
-            <div class="d-flex">
+            <div class="settings-icons-wrapper">
                 <div>
                     <a href="#">
-                        <img src="{{ asset('admin/assets/images/cart.svg') }}" width="20" style="margin-right: 10px;">
+                        <img src="{{ asset('admin/assets/images/cart.svg') }}">
                         <span>Магазин</span>
                     </a>
                 </div>
