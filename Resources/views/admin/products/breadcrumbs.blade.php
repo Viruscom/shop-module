90a1fc8904b727dbebc4d06781199dd70132c399
h.chobanov <hristiqn.chobanov@gmail.com>
2023-06-05 15:15:15 +0300

    sdfdsf

--- Resources/views/admin/products/breadcrumbs.blade.php
+++ Resources/views/admin/products/breadcrumbs.blade.php
@@ -7,10 +7,10 @@
         <li>
             <a href="{{ route('admin.products.index') }}" class="text-black">{{ __('shop::admin.products.index') }}</a>
         </li>
-        @if(Request::segment(5) !== null && url()->current() === route('admin.products.create', ['id' => Request::segment(5)]))
+        @if(Request::segment(5) !== null && url()->current() === route('admin.products.create', ['category_id' => Request::segment(5)]))
             <li>
                 <i class="fa fa-angle-right"></i>
-                <a href="{{ route('admin.products.create') }}" class="text-purple">{{ __('shop::admin.products.create') }}</a>
+                <a href="{{ route('admin.products.create', ['category_id' => Request::segment(5)]) }}" class="text-purple">{{ __('shop::admin.products.create') }}</a>
             </li>
         @elseif(Request::segment(4) !== null && url()->current() === route('admin.products.edit', ['id' => Request::segment(4)]))
             <li>
