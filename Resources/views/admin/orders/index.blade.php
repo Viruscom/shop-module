1b334b093d866c6c7c522fc7709b7f17c0312b14
h.chobanov <hristiqn.chobanov@gmail.com>
2023-07-11 14:23:21 +0300

    Orders index page change, MailChimp package added, MyPosPaymentSeeder fix

--- Resources/views/admin/orders/index.blade.php
+++ Resources/views/admin/orders/index.blade.php
@@ -1,68 +1,30 @@
 @php use Carbon\Carbon; @endphp@extends('layouts.admin.app')
 
 @section('styles')
-    <link href="{{ asset('admin/css/select2.min.css') }}" rel="stylesheet"/>
-    {{-- <link href="{{ asset('admin/css/jquery.dataTables.min.css') }}" rel="stylesheet" /> --}}
-    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/v/dt/dt-1.10.22/r-2.2.6/datatables.min.css"/>
-    <link href="{{ asset('admin/css/fixedHeader.dataTables.min.css') }}" rel="stylesheet"/>
+    <link href="{{ asset('admin/assets/css/fixedHeader.dataTables.min.css') }}" rel="stylesheet"/>
+    <style>
+        #example {
+            font-size: 13px;
+        }
+    </style>
 @endsection
 @section('scripts')
-    <script src="{{ asset('admin/js/select2.min.js') }}"></script>
-    <script src="{{ asset('admin/js/bootstrap-confirmation.js') }}"></script>
-    <script src="{{ asset('admin/js/jquery.dataTables.min.js') }}"></script>
-    <script src="{{ asset('admin/js/dataTables.fixedHeader.min.js') }}"></script>
+    <script src="{{ asset('admin/assets/js/jquery.dataTables.min.js') }}"></script>
+    <script src="{{ asset('admin/assets/js/dataTables.fixedHeader.min.js') }}"></script>
     <script>
-        $('[data-toggle=confirmation]').confirmation({
-            rootSelector: '[data-toggle=confirmation]',
-            container: 'body',
-        });
-        $(".select2").select2({language: "bg"});
         $(document).ready(function () {
-            $('[data-toggle="popover"]').popover({
-                placement: 'auto',
-                trigger: 'hover',
-                html: true,
-            }).on("show.bs.popover", function () {
-                $(this).data("bs.popover").tip().css("max-width", "80%");
-            });
-            // Setup - add a text input to each footer cell
-            $('#example thead tr').clone(true).appendTo('#example thead');
-            $('#example thead tr:eq(1) th').each(function (i) {
-                var title = $(this).text();
-                $(this).html('<input type="text" class="datatable-filter-input" placeholder="филтрирай по ' + title + '" style="width:100%;" />');
 
-                $('input', this).on('keyup change', function () {
-                    if (table.column(i).search() !== this.value) {
-                        table
-                            .column(i)
-                            .search(this.value)
-                            .draw();
-                    }
-                });
-            });
+            var options = {
+                withSortableRow: true,
+                sortableRowFromColumn: 1,
+                sortableRowToColumn: 9,
+                withDynamicSort: true,
+                dynamicSortFromColumn: 1,
+                dynamicSortToColumn: 9,
+                rowsPerPage: 50
+            }
+            initDatatable('example', options);
 
-            var table = $('#example').DataTable({
-                "order": [[7, "desc"]],
-                orderCellsTop: true,
-                fixedHeader: true,
-                language: {
-                    "sProcessing": "Обработка на резултатите...",
-                    "sLengthMenu": "Показване на _MENU_ резултата",
-                    "sZeroRecords": "Няма намерени резултати",
-                    "sInfo": "Показване на резултати от _START_ до _END_ от общо _TOTAL_",
-                    "sInfoEmpty": "Показване на резултати от 0 до 0 от общо 0",
-                    "sInfoFiltered": "(филтрирани от общо _MAX_ резултата)",
-                    "sInfoPostFix": "",
-                    "sSearch": "Търсене:",
-                    "sUrl": "",
-                    "oPaginate": {
-                        "sFirst": "Първа",
-                        "sPrevious": "Предишна",
-                        "sNext": "Следваща",
-                        "sLast": "Последна"
-                    }
-                }
-            });
         });
     </script>
 @endsection
@@ -91,19 +53,19 @@
     </div>
 
     <div class="row">
-        <div class="col-md-12 overflow-auto">
+        <div class="col-md-12" style="overflow: auto;">
             <table id="example" class="table table-striped">
                 <thead>
                 <tr>
                     <th style="max-width: 50px">№</th>
-                    <th>Дата</th>
+                    <th>Дата и час</th>
                     <th>Статус на изпълнение</th>
-                    <th>Статус на плащане</th>
+                    <th>{{ __('shop::admin.orders.payment_status') }}</th>
                     <th>Клиент</th>
-                    <th>Адрес на доставка</th>
+                    <th>{{ __('shop::admin.orders.delivery_address') }}</th>
                     <th>Сума</th>
-                    <th>Метод на плащане</th>
-                    <th>Метод на доставка</th>
+                    <th>{{ __('shop::admin.orders.payment') }}</th>
+                    <th>{{ __('shop::admin.orders.delivery_method') }}</th>
                     <th>{{ __('admin.actions') }}</th>
                 </tr>
                 </thead>
@@ -129,7 +91,7 @@
                             <td>{{ $order->street .', '. $order->street_number }}</td>
                             <td>
                                 <div>
-                                    Общо: {{ $order->total }} лв.
+                                    Общо: {{ $order->total }} лв.<br>
                                     <span>Отстъпки: <strong class="text-purple">-{{ $order->discounts_to_apply }}</strong> лв.</span><br>
                                     <span>Общо с отстъпки и ДДС: {{ $order->total_discounted }} лв.</span>
                                 </div>
