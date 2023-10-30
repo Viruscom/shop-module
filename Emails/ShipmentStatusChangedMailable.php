22f5a131029c8b031d7b25e6bcd7b71cbdd74d36
h.chobanov <hristiqn.chobanov@gmail.com>
2023-10-30 08:08:45 +0200

    sdfdsd

--- Emails/ShipmentStatusChangedMailable.php
+++ Emails/ShipmentStatusChangedMailable.php
@@ -1,35 +1,35 @@
 <?php
 
-namespace Modules\Shop\Emails;
+    namespace Modules\Shop\Emails;
 
-use App\Models\LawPages\LawPage;
-use App\Models\Settings\ShopSetting;
-use App\Models\Settings\Social;
-use Illuminate\Bus\Queueable;
-use Illuminate\Mail\Mailable;
-use Illuminate\Queue\SerializesModels;
-use Modules\Shop\Entities\Orders\Order;
+    use App\Models\LawPages\LawPage;
+    use App\Models\Settings\ShopSetting;
+    use App\Models\Settings\Social;
+    use Illuminate\Bus\Queueable;
+    use Illuminate\Mail\Mailable;
+    use Illuminate\Queue\SerializesModels;
+    use Modules\Shop\Entities\Orders\Order;
 
-class ShipmentStatusChangedMailable extends Mailable
-{
-    use Queueable, SerializesModels;
-
-    public $order;
-    public $shopSettings;
-    public $socialLinks;
-    public $lawPages;
-
-    public function __construct(Order $order)
+    class ShipmentStatusChangedMailable extends Mailable
     {
-        $this->order        = $order;
-        $this->shopSettings = ShopSetting::get();
-        $this->socialLinks  = Social::getSettings();
-        $this->lawPages     = LawPage::getLawPages();
-    }
+        use Queueable, SerializesModels;
 
-    public function build()
-    {
-        return $this->view('shop::emails.orders.shipment_status_changed')
-            ->with(['order' => $this->order, 'shopSettings' => $this->shopSettings, 'socialLinks' => $this->socialLinks, 'lawPages' => $this->lawPages]);
+        public $order;
+        public $shopSettings;
+        public $socialLinks;
+        public $lawPages;
+
+        public function __construct(Order $order)
+        {
+            $this->order        = $order;
+            $this->shopSettings = ShopSetting::get();
+            $this->socialLinks  = Social::getSettings();
+            $this->lawPages     = LawPage::getLawPages();
+        }
+
+        public function build()
+        {
+            return $this->view('shop::emails.orders.shipment_status_change')
+                ->with(['order' => $this->order, 'shopSettings' => $this->shopSettings, 'socialLinks' => $this->socialLinks, 'lawPages' => $this->lawPages]);
+        }
     }
-}
