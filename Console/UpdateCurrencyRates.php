59d124807319e0932a4e48b695b30757d0051714
h.chobanov <hristiqn.chobanov@gmail.com>
2023-11-20 13:52:24 +0200

    dsdfsdf

--- Console/UpdateCurrencyRates.php
+++ Console/UpdateCurrencyRates.php
@@ -1,41 +1,41 @@
 <?php
 
-namespace App\Console\Commands;
+    namespace Modules\Shop\Console;
 
-use Carbon\Carbon;
-use Illuminate\Console\Command;
-use Illuminate\Support\Facades\Http;
-use Modules\Shop\Entities\Settings\CurrencyRate;
+    use Carbon\Carbon;
+    use Illuminate\Console\Command;
+    use Illuminate\Support\Facades\Http;
+    use Modules\Shop\Entities\Settings\CurrencyRate;
 
-class UpdateCurrencyRates extends Command
-{
-    protected $signature   = 'update:currency-rates';
-    protected $description = 'Update currency rates from API';
-
-    public function __construct()
+    class UpdateCurrencyRates extends Command
     {
-        parent::__construct();
-    }
+        protected $signature   = 'update:currency-rates';
+        protected $description = 'Update currency rates from API';
 
-    public function handle()
-    {
-        $response = Http::get('https://your-exchange-rate-api-url'); // Replace with your API URL
-        $data     = $response->json();
-
-        if ($data['result'] !== 'success') {
-            $this->error('API call failed');
-
-            return;
+        public function __construct()
+        {
+            parent::__construct();
         }
 
-        $now = Carbon::now();
-        foreach ($data['conversion_rates'] as $currency => $rate) {
-            CurrencyRate::updateOrCreate(
-                ['base_currency' => $data['base_code'], 'target_currency' => $currency],
-                ['rate' => $rate, 'last_update' => $now, 'next_update' => Carbon::createFromTimestamp($data['time_next_update_unix'])]
-            );
-        }
+        public function handle()
+        {
+            $response = Http::get('https://your-exchange-rate-api-url'); // Replace with your API URL
+            $data     = $response->json();
 
-        $this->info('Currency rates updated successfully');
+            if ($data['result'] !== 'success') {
+                $this->error('API call failed');
+
+                return;
+            }
+
+            $now = Carbon::now();
+            foreach ($data['conversion_rates'] as $currency => $rate) {
+                CurrencyRate::updateOrCreate(
+                    ['base_currency' => $data['base_code'], 'target_currency' => $currency],
+                    ['rate' => $rate, 'last_update' => $now, 'next_update' => Carbon::createFromTimestamp($data['time_next_update_unix'])]
+                );
+            }
+
+            $this->info('Currency rates updated successfully');
+        }
     }
-}
