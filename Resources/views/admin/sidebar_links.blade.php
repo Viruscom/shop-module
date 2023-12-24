3d211b22e7b04e792a8d8030b3fa2df4a01e74b6
h.chobanov <hristiqn.chobanov@gmail.com>
2023-12-24 09:20:55 +0200

    dsfsdf

--- Resources/views/admin/sidebar_links.blade.php
+++ Resources/views/admin/sidebar_links.blade.php
@@ -1,30 +1,4 @@
 @php use App\Helpers\WebsiteHelper;use Illuminate\Http\Request; @endphp
-<div class="panel panel-default">
-    <div class="panel-heading" role="tab" id="headingFour" data-toggle="collapse" data-parent="#accordionMenu" href="#collapseFour" aria-expanded="false" aria-controls="collapseThree">
-        <h4 class="panel-title">
-            <a>
-                <i class="far fa-list-alt"></i> <span>{!! trans('shop::admin.products.index') !!}</span>
-            </a>
-        </h4>
-    </div>
-    <div id="collapseFour" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingFour">
-        <div class="panel-body">
-            <ul class="nav">
-                <li><a href="{{ route('admin.brands.index') }}" class="{{ WebsiteHelper::isActiveRoute('admin.brands.*') ? 'active' : '' }}"><i class="fas fa-copyright"></i> <span>{!! trans('shop::admin.product_brands.index') !!}</span></a></li>
-                <li><a href="{{ route('admin.product-categories.index') }}" class="{{ WebsiteHelper::isActiveRoute('admin.product-categories.*') ? 'active' : '' }}"><i class="fas fa-outdent"></i> <span>{!! trans('shop::admin.product_categories.index') !!}</span></a></li>
-                <li><a href="{{ route('admin.products.index') }}" class="{{ WebsiteHelper::isActiveRoute('admin.products.*') && !WebsiteHelper::isActiveRoute('admin.product_characteristics.*') ? 'active' : '' }}"><i class="far fa-list-alt"></i> <span>{!! trans('shop::admin.products.index') !!}</span></a></li>
-                @if(array_key_exists('RetailObjectsRestourant', $activeModules))
-                    <li><a href="{{ route('admin.product.additives.index') }}" class="{{ WebsiteHelper::isActiveRoute('admin.product.additives.*') ? 'active' : '' }}"><img src="{{ asset('admin/assets/images/ingredients_dishes.svg') }}" width="20"> <span>{!! trans('retailobjectsrestourant::admin.product_additives.index') !!}</span></a></li>
-                @endif
-                <li><a href="{{ route('admin.product-attributes.index') }}" class="{{ WebsiteHelper::isActiveRoute('admin.product-attributes.*') ? 'active' : '' }}"><img src="{{ asset('admin/assets/images/product_attribute.svg') }}" alt="@lang('shop::admin.product_attributes.index')" width="18" style="margin-right: 12px;"> <span>{!! trans('shop::admin.product_attributes.index') !!}</span></a></li>
-                <li><a href="{{ route('admin.product_characteristics.index') }}" class="{{ WebsiteHelper::isActiveRoute('admin.product_characteristics.*') ? 'active' : '' }}"><img src="{{ asset('admin/assets/images/product_characteristics.svg') }}" alt="@lang('shop::admin.product_characteristics.index')" width="18" style="margin-right: 12px;"> <span>{!! trans('shop::admin.product_characteristics.index') !!}</span></a></li>
-                <li><a href="{{ route('admin.product-combinations.index') }}" class="{{ WebsiteHelper::isActiveRoute('admin.product-combinations.*') ? 'active' : '' }}"><img src="{{ asset('admin/assets/images/product_combinations.svg') }}" alt="@lang('shop::admin.product_combinations.index')" width="18" style="margin-right: 12px;"> <span>{!! trans('shop::admin.product_combinations.index') !!}</span></a></li>
-                <li><a href="{{ route('admin.product-collections.index') }}" class="{{ WebsiteHelper::isActiveRoute('admin.product-collections.*') ? 'active' : '' }}"><i class="fas fa-layer-group"></i> <span>{!! trans('shop::admin.product_collections.index') !!}</span></a></li>
-                <li><a href="{{ route('admin.product-stocks.index') }}" class="{{ WebsiteHelper::isActiveRoute('admin.product-stocks.*') ? 'active' : '' }}"><img src="{{ asset('admin/assets/images/product_stock.svg') }}" width="20" height="25"><span>{!! trans('shop::admin.product_stocks.index') !!}</span></a></li>
-            </ul>
-        </div>
-    </div>
-</div>
 
 <div class="panel panel-default">
     <div class="panel-heading" role="tab" id="headingFive" data-toggle="collapse" data-parent="#accordionMenu" href="#collapseFive" aria-expanded="false" aria-controls="collapseFour">
@@ -47,6 +21,33 @@
                 {{--                <li><a href="https://main.reklamnipodaraci.com/admin/shop/h-18-reports"><i class="fas fa-user-friends"></i>--}}
                 {{--                        <span>{{ __('shop::admin.h18_reports.index') }}</span></a></li>--}}
                 <li><a href="{{ route('admin.shop.settings.index') }}" class="{{ WebsiteHelper::isActiveRoute('admin.shop.settings.*') ? 'active' : '' }}"><img src="{{ asset('admin/assets/images/shop_settings.svg') }}" width="20"> <span>{!! trans('shop::admin.shop.settings_index') !!}</span></a></li>
+            </ul>
+        </div>
+    </div>
+</div>
+
+<div class="panel panel-default">
+    <div class="panel-heading" role="tab" id="headingFour" data-toggle="collapse" data-parent="#accordionMenu" href="#collapseFour" aria-expanded="false" aria-controls="collapseThree">
+        <h4 class="panel-title">
+            <a>
+                <i class="far fa-list-alt"></i> <span>{!! trans('shop::admin.products.index') !!}</span>
+            </a>
+        </h4>
+    </div>
+    <div id="collapseFour" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingFour">
+        <div class="panel-body">
+            <ul class="nav">
+                <li><a href="{{ route('admin.brands.index') }}" class="{{ WebsiteHelper::isActiveRoute('admin.brands.*') ? 'active' : '' }}"><i class="fas fa-copyright"></i> <span>{!! trans('shop::admin.product_brands.index') !!}</span></a></li>
+                <li><a href="{{ route('admin.product-categories.index') }}" class="{{ WebsiteHelper::isActiveRoute('admin.product-categories.*') ? 'active' : '' }}"><i class="fas fa-outdent"></i> <span>{!! trans('shop::admin.product_categories.index') !!}</span></a></li>
+                <li><a href="{{ route('admin.products.index') }}" class="{{ WebsiteHelper::isActiveRoute('admin.products.*') && !WebsiteHelper::isActiveRoute('admin.product_characteristics.*') ? 'active' : '' }}"><i class="far fa-list-alt"></i> <span>{!! trans('shop::admin.products.index') !!}</span></a></li>
+                @if(array_key_exists('RetailObjectsRestourant', $activeModules))
+                    <li><a href="{{ route('admin.product.additives.index') }}" class="{{ WebsiteHelper::isActiveRoute('admin.product.additives.*') ? 'active' : '' }}"><img src="{{ asset('admin/assets/images/ingredients_dishes.svg') }}" width="20"> <span>{!! trans('retailobjectsrestourant::admin.product_additives.index') !!}</span></a></li>
+                @endif
+                <li><a href="{{ route('admin.product-attributes.index') }}" class="{{ WebsiteHelper::isActiveRoute('admin.product-attributes.*') ? 'active' : '' }}"><img src="{{ asset('admin/assets/images/product_attribute.svg') }}" alt="@lang('shop::admin.product_attributes.index')" width="18" style="margin-right: 12px;"> <span>{!! trans('shop::admin.product_attributes.index') !!}</span></a></li>
+                <li><a href="{{ route('admin.product_characteristics.index') }}" class="{{ WebsiteHelper::isActiveRoute('admin.product_characteristics.*') ? 'active' : '' }}"><img src="{{ asset('admin/assets/images/product_characteristics.svg') }}" alt="@lang('shop::admin.product_characteristics.index')" width="18" style="margin-right: 12px;"> <span>{!! trans('shop::admin.product_characteristics.index') !!}</span></a></li>
+                <li><a href="{{ route('admin.product-combinations.index') }}" class="{{ WebsiteHelper::isActiveRoute('admin.product-combinations.*') ? 'active' : '' }}"><img src="{{ asset('admin/assets/images/product_combinations.svg') }}" alt="@lang('shop::admin.product_combinations.index')" width="18" style="margin-right: 12px;"> <span>{!! trans('shop::admin.product_combinations.index') !!}</span></a></li>
+                <li><a href="{{ route('admin.product-collections.index') }}" class="{{ WebsiteHelper::isActiveRoute('admin.product-collections.*') ? 'active' : '' }}"><i class="fas fa-layer-group"></i> <span>{!! trans('shop::admin.product_collections.index') !!}</span></a></li>
+                <li><a href="{{ route('admin.product-stocks.index') }}" class="{{ WebsiteHelper::isActiveRoute('admin.product-stocks.*') ? 'active' : '' }}"><img src="{{ asset('admin/assets/images/product_stock.svg') }}" width="20" height="25"><span>{!! trans('shop::admin.product_stocks.index') !!}</span></a></li>
             </ul>
         </div>
     </div>
